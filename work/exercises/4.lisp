93fa456074878dec582a38f6af331c128353fb79
Joe Van Dyk <joe@fixieconsulting.com>
2009-08-27 04:39:30 -0700

    Couple changes

--- work/exercises/4.lisp
+++ work/exercises/4.lisp
@@ -1,15 +1,18 @@
 (load "check-equal.lisp")
 
 ; Finds the number of elements in a list
-
+; Not sure if this is the best way to do this.
+; I also tend to always use recursion?
 (defun number-of-elements (lst)
+  ; Sums up the rest of the elements of the list
   (defun current-count (lsta i)
-    (if null (lsta)
+    (if (null lsta)
       i
       (current-count (cdr lsta) (+ 1 i))))
-  (current-count (lst 0)))
+  ; Starts off the list count with 0
+  (current-count lst 0 ))
 
 
 (check-equal 5 (number-of-elements '(1 2 3 4 5)))
-(check-equal 0 (number-of-elements '()))
+(check-equal 3 (number-of-elements '()))
 (check-equal 1 (number-of-elements '(1)))
--- work/exercises/check-equal.lisp
+++ work/exercises/check-equal.lisp
@@ -1,5 +1,24 @@
+; There's gotta be a better way to do this
+(let ()
+  (setq last-result :success)
+  (defun print-result (current-result &rest messages)
+    (let ()
+      (if (or (equal last-result :failure) 
+              (equal current-result :failure))
+        (format t "~%"))
+      (if (equal current-result :success)
+        (format t ".")
+        (format t " *** ERROR: ~?" (car messages) (cdr messages)))
+      (setq last-result current-result))))
+
+(defun print-success ()
+  (print-result :success))
+
+(defun print-failure (expected actual)
+  (print-result :failure "Expected ~S, Got ~S" expected actual))
+
 ; Checks to see if two things are equal
 (defun check-equal (expected actual)
   (if (equal expected actual)
-    (format t ".")
-    (format t "Expected ~S, Got ~S" expected actual)))
+    (print-success)
+    (print-failure expected actual)))
